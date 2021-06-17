ed9ad21b445bd556affa92c23541d5f01c226d00
Yasin <yasinabdussaburr@gmail.com>
2021-06-16 21:28:22 -0400

    commit tree e0520b89902acf82a75fbc037ffe9fa355e1b58a

---
+++ index.js
@@ -0,0 +1,7 @@
+const receivesAFunction = (x) => (x());
+const returnsANamedFunction = () =>  function named () {
+    return 1-1
+}//const add = (x, y) => x - y
+const returnsAnAnonymousFunction = () => function () {
+    return 2+2;
+}
